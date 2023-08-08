14a9d1c5d4c195dcf8ad1b2c336c2ce13c977c06
Ayosef <syrivera@uc.cl>
2023-08-08 10:16:16 -0400

    Add a function to create and display the player and computer grid on the webpage

---
+++ src/modules/dom.js
@@ -0,0 +1,24 @@
+// Complete the grid on the website
+export default function completeDomGrid() {
+  // Get the player and computer grid
+  const playerGrid = document.querySelector(".player-grid");
+  const computerGrid = document.querySelector(".computer-grid");
+
+  // Create the grid for the player and computer
+  createDomGrid(playerGrid);
+  createDomGrid(computerGrid);
+}
+
+function createDomGrid(grid, size = 10) {
+  // Create a size x size grid
+  for (let i = 0; i < size; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.add("grid-row");
+    for (let j = 0; j < size; j++) {
+      const newCol = document.createElement("div");
+      newCol.classList.add("grid-col");
+      newRow.appendChild(newCol);
+    }
+    grid.appendChild(newRow);
+  }
+}
