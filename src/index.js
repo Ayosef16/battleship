cfd1c815e801732c5ff84803605a0d81fc69b0f2
Ayosef <syrivera@uc.cl>
2023-08-10 17:56:00 -0400

    Show the current player displayed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "normalize.css";
 import "./style.css";
-import { completeDomGrid, createEventListener } from "./modules/dom";
+import {
+  completeDomGrid,
+  createEventListener,
+  displayCurrentPlayer,
+} from "./modules/dom";
 import gameLoopFactory from "./modules/gameloop";
 
 const game = gameLoopFactory();
 
 completeDomGrid();
 createEventListener(game);
+displayCurrentPlayer(game.getCurrentPlayerName());
