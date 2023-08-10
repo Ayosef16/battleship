d68c639b2b8af8c208235fe6124c20180aa253e3
Ayosef <syrivera@uc.cl>
2023-08-10 09:51:33 -0400

    Add the player and computer grid, and event listeners

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "normalize.css";
 import "./style.css";
-import completeDomGrid from "./modules/dom";
+import { completeDomGrid, createEventListener } from "./modules/dom";
+import gameLoopFactory from "./modules/gameloop";
+
+const game = gameLoopFactory();
 
 completeDomGrid();
+createEventListener(game);
